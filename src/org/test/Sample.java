6fd4a36f758574a3f1c2fb4008ebff29f2d67f06
Godfreysg09 <godfreysg09@gmail.com>
2022-08-02 23:20:14 +0530

    Task Completed

---
+++ src/org/test/Sample.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Sample {
+
+	private void method1() {
+		System.out.println("1234567890");
+	}
+	
+	public static void main(String[] args) {
+		
+	
+	Sample a = new Sample();
+	
+	a.method1();
+	
+	}
+
+}
